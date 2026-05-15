0ab86b7b96ccabb0a444169d97141e99abe26d93
gonzaagoni <gonzaagoni@gmail.com>
2026-05-15 04:56:29 -0300

    Add files via upload

---
+++ TA-TE-TI/server.js
@@ -0,0 +1,354 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+const crypto = require("crypto")
+
+const app = express()
+
+const server = http.createServer(app)
+
+const io = new Server(server)
+
+app.use(express.static("public"))
+
+app.get("/sala/:id",(req,res)=>{
+
+res.sendFile(__dirname + "/public/index.html")
+
+})
+
+const salas = {}
+
+io.on("connection",(socket)=>{
+
+socket.on("joinRoom",(data)=>{
+
+const room = data.room
+
+socket.join(room)
+
+if(!salas[room]){
+
+salas[room] = {
+jugadores:[],
+turno:"X",
+tablero:["","","","","","","","",""],
+scoreX:0,
+scoreO:0,
+estado:"esperando"
+}
+
+}
+
+const sala = salas[room]
+
+const yaExiste =
+sala.jugadores.find(
+j=>j.id == socket.id
+)
+
+if(yaExiste) return
+
+if(sala.jugadores.length >= 2){
+
+socket.emit("roomFull")
+
+return
+
+}
+
+const jugador = {
+id:socket.id,
+nombre:data.nombre,
+foto:data.foto,
+simbolo:
+sala.jugadores.length == 0
+? "X"
+: "O",
+conectado:true
+}
+
+sala.jugadores.push(jugador)
+
+if(sala.jugadores.length == 2){
+
+sala.estado = "jugando"
+
+}else{
+
+sala.estado = "esperando"
+
+}
+
+socket.emit("playerData",jugador)
+
+io.to(room).emit(
+"playersUpdate",
+sala.jugadores
+)
+
+io.to(room).emit(
+"statusUpdate",
+sala.estado
+)
+
+io.to(room).emit(
+"boardUpdate",
+sala.tablero
+)
+
+io.to(room).emit(
+"scoreUpdate",
+{
+scoreX:sala.scoreX,
+scoreO:sala.scoreO
+}
+)
+
+io.to(room).emit(
+"turnUpdate",
+sala.turno
+)
+
+})
+
+socket.on("move",(data)=>{
+
+const sala = salas[data.room]
+
+if(!sala) return
+
+const jugador =
+sala.jugadores.find(
+j=>j.id == socket.id
+)
+
+if(!jugador) return
+
+if(jugador.simbolo != sala.turno)
+return
+
+if(sala.tablero[data.index] != "")
+return
+
+sala.tablero[data.index] =
+jugador.simbolo
+
+io.to(data.room).emit(
+"boardUpdate",
+sala.tablero
+)
+
+const ganador =
+verificarGanador(
+sala.tablero
+)
+
+if(ganador){
+
+if(ganador == "X"){
+
+sala.scoreX++
+
+}else{
+
+sala.scoreO++
+
+}
+
+io.to(data.room).emit(
+"scoreUpdate",
+{
+scoreX:sala.scoreX,
+scoreO:sala.scoreO
+}
+)
+
+io.to(data.room).emit(
+"winner",
+ganador
+)
+
+setTimeout(()=>{
+
+reiniciarTablero(
+data.room
+)
+
+},1500)
+
+return
+
+}
+
+const empate =
+sala.tablero.every(c=>c!="")
+
+if(empate){
+
+io.to(data.room).emit(
+"draw"
+)
+
+setTimeout(()=>{
+
+reiniciarTablero(
+data.room
+)
+
+},1500)
+
+return
+
+}
+
+sala.turno =
+sala.turno == "X"
+? "O"
+: "X"
+
+io.to(data.room).emit(
+"turnUpdate",
+sala.turno
+)
+
+})
+
+socket.on("resetScore",(room)=>{
+
+if(!salas[room]) return
+
+salas[room].scoreX = 0
+salas[room].scoreO = 0
+
+io.to(room).emit(
+"scoreUpdate",
+{
+scoreX:0,
+scoreO:0
+}
+)
+
+})
+
+socket.on("chat",(data)=>{
+
+if(
+!data.mensaje ||
+data.mensaje.length > 200
+) return
+
+io.to(data.room).emit(
+"chat",
+{
+nombre:data.nombre,
+mensaje:data.mensaje
+}
+)
+
+})
+
+socket.on("disconnect",()=>{
+
+for(const room in salas){
+
+const sala = salas[room]
+
+sala.jugadores =
+sala.jugadores.filter(
+j=>j.id != socket.id
+)
+
+if(sala.jugadores.length == 0){
+
+delete salas[room]
+
+continue
+
+}
+
+sala.estado = "esperando"
+
+io.to(room).emit(
+"statusUpdate",
+"esperando rival"
+)
+
+io.to(room).emit(
+"playersUpdate",
+sala.jugadores
+)
+
+}
+
+})
+
+})
+
+function reiniciarTablero(room){
+
+if(!salas[room]) return
+
+salas[room].tablero =
+["","","","","","","","",""]
+
+salas[room].turno = "X"
+
+io.to(room).emit(
+"boardUpdate",
+salas[room].tablero
+)
+
+io.to(room).emit(
+"turnUpdate",
+"X"
+)
+
+}
+
+function verificarGanador(tablero){
+
+const combinaciones = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+for(let c of combinaciones){
+
+const a = tablero[c[0]]
+const b = tablero[c[1]]
+const d = tablero[c[2]]
+
+if(a != "" && a == b && b == d){
+
+return a
+
+}
+
+}
+
+return null
+
+}
+
+app.get("/crear",(req,res)=>{
+
+const id =
+crypto.randomBytes(3)
+.toString("hex")
+
+res.redirect("/sala/"+id)
+
+})
+
+server.listen(3000,()=>{
+
+console.log("Servidor iniciado")
+
+})
